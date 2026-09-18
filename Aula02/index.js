b672ab90237a5ee20b9076872f6e7d6c66af767b
Eduarda Augusto <eduardaugustoo@icloud.com>
2026-09-18 10:36:04 -0300

    Add files via upload

---
+++ Aula02/index.js
@@ -0,0 +1,12 @@
+var cor = "azul"
+var cor = "vermelho";
+console.log(cor); 
+
+let fruta ="Manga";
+fruta = "uva";
+console.log(fruta);
+
+const pi = 3.14;
+// pi = 3.15; pode da um ERRO 
+
+console.log(pi);
