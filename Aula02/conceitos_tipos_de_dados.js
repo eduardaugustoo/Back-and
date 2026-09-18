1e7a5fe3b5b0f27e2a83c973bbb73abca0a62091
Eduarda <eduardaugustoo@icloud.com>
2026-09-18 11:31:06 -0300

    exercicio_01.js

---
+++ Aula02/conceitos_tipos_de_dados.js
@@ -0,0 +1,29 @@
+//Tipos de dados 
+//no javascript, tudo o que armazenamos em variáveis tem um tipo. esses tipos definem o que podemos fazer com os valores.
+
+//Principais tipos de dados:
+//string (texto)
+//number (número)
+//boolean (verdadeiro ou falso)
+//object (objetos que agrupam informações)
+//Array (lista de valores)
+//null (valor vazio)
+//undefined (quando algo não foi definido)
+
+let nome = "jarvis";
+let mensagem = 'Ola, mundo'
+
+console.log (nome);
+console.log(mensagem);
+
+let saudacao = "ola,"  + nome + "!";
+console.log (saudacao); //Exibe "ola, jarvis"
+
+//typeof 
+// O Typeof serve parra descobrir um tipo de um valor ou variavel
+
+let nomeDois  = "2";
+console.log(typeof nomeDois);
+
+let soma = 10+ 5;
+console.log(soma);
---
+++ Aula02/exercicio_01.js
@@ -0,0 +1,10 @@
+//Atividade number
+//crie uma variavel chama anoaAtual e outra chamada anoNascimento.
+//calcule e exiba a idade no(a) console 
+
+
+let anoAtual = "2026"
+let anoNascimento = "2009"
+let idade = anoAtual - anoNascimento
+
+console.log (idade);
