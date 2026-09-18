bd7424611f5516b3e86a3d04d888b1c388bfc979
Eduarda <eduardaugustoo@icloud.com>
2026-09-18 12:00:34 -0300

    back-and

--- Aula02/conceitos_tipos_de_dados.js
+++ Aula02/conceitos_tipos_de_dados.js
@@ -27,3 +27,18 @@ console.log(typeof nomeDois);
 
 let soma = 10+ 5;
 console.log(soma);
+
+
+//boolean (verdadeiro ou falso)
+//Um boolean pode ter apenas dois valores: true (verdadeiro) ou false(falso)
+
+let maiorDeIdade = true;
+let menorDeIdade = false;
+
+console.log(maiorDeIdade); //exibe true
+console.log(menorDeIdade); //exibe false
+let idade = 16;
+let podeDirigir = idade >=18;
+console.log(podeDirigir)
+
+
